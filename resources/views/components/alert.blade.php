0953f277345916a43228a19353eca373a3ad0585
AhmadMaulanaFaiz <izuuu3309@gmail.com>
2025-03-21 15:01:01 +0700

    [Pertemuan 3] Membuat Layout dan Component

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,24 @@
+<br>
+<button type="button" class="btn btn-primary" id="liveAlertBtn">tombol alert</button>
+<div id="liveAlertPlaceholder"></div>
+<script>
+    const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible fade show" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
+
+const alertTrigger = document.getElementById('liveAlertBtn')
+if (alertTrigger) {
+  alertTrigger.addEventListener('click', () => {
+    appendAlert('alert sudah berhasil', 'success')
+  })
+}
+</script>
